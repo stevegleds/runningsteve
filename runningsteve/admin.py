e13dac68f3f652813019c384c29853fbfef2017f
stevegleds <stevegleds@gmail.com>
2018-10-14 21:42:27 +0100

    14 October 2018 See Todo : think about the model and links between race/ruunner and consider fields for distance Created templates with simple index.html. Problem: database tables not created. Next step : create tables somehow Update: problem was I needed to specify 'runningsteve' folder (app) that had the models.py e.g. python manage.py makemigrations runningsteve Now adding data but need to make it work the way I like.

--- runningsteve/admin.py
+++ runningsteve/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Runner, Race
+from runningsteve.models import Runner, Race
 
 admin.site.register(Runner)
 admin.site.register(Race)
--- runningsteve/models.py
+++ runningsteve/models.py
@@ -7,9 +7,9 @@ from django.utils import timezone
 
 
 class Runner(models.Model):
-    first_name = models.CharField('First Name', max_length=20, null=True)
-    second_name = models.CharField('Second Name', max_length=20, null=True)
-    last_name = models.CharField('Last Name', max_length=20, null=True)
+    first_name = models.CharField('First Name', max_length=25, null=True)
+    second_name = models.CharField('Second Name', max_length=25)
+    last_name = models.CharField('Last Name', max_length=25, null=True)
     gender = models.CharField('Gender', max_length=6, null=True)
     date_of_birth = models.DateField('Date of Birth', null=True)
 
@@ -24,15 +24,16 @@ class Runner(models.Model):
 class Race(models.Model):
     race_name = models.CharField('Race', max_length=20)
     route_name = models.CharField('Route', max_length=20, null=True)
-    route_length = models.FloatField('Miles', null=True)
+    route_distance = models.FloatField('Distance', null=True)
+    route_units = models.CharField('Units', max_length=5)
     terrain = models.CharField('Terrain', max_length=20, null=True)
     race_date = models.DateTimeField('Date', null=True)
     runner = models.ForeignKey(Runner, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         # return self.race_name, self.race_date, self.route_length, self.terrain, self.race_date, self.runner
-        return f"{self.race_name, self.route_name, self.route_length, self.terrain, self.race_date, self.runner}"
+        return f"{self.race_name, self.route_name, self.route_distance, self.terrain, self.race_date, self.runner}"
 
     def __repr__(self):
         # return self.race_name, self.race_date, self.route_length, self.terrain, self.race_date, self.runner
-        return f"{self.race_name, self.route_name, self.route_length, self.terrain, self.race_date, self.runner}"
+        return f"{self.race_name, self.route_name, self.route_distance, self.terrain, self.race_date, self.runner}"
