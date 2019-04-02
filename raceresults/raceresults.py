8974864912dde49b2af8322c427430795d86c9b5
stevegleds <stevegleds@gmail.com>
2019-04-02 22:20:17 +0100

    02 April 2019 Added pace column using pace function. Next: change format of column to show as 8:45 not 00:08:45

--- raceresults/raceresults.py
+++ raceresults/raceresults.py
@@ -5,11 +5,19 @@ data_dir = os.getcwd()
 results_file = data_dir+'\\racessummary20190329.xlsx'
 
 
+def pace(time):
+    return pd.to_timedelta(time) / 2
+
+
 def main():
     dfraceresults = pd.read_excel(results_file, encoding='ISO-8859-1')
     print(dfraceresults.head())
     print("results file:", results_file)
     dfraceresults.to_html("raceresults.html")
+    response = input("Calculate pace? Y/y \n")
+    if response.lower() == 'y':
+        dfraceresults['Pace'] = pd.to_timedelta(dfraceresults.Time) / dfraceresults.Miles
+        dfraceresults.to_html("raceresults.html")
 
 
 if __name__ == "__main__":
