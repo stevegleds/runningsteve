1491031006bc3dde580125a18146bfe704b1b393
Steve Gledhill <stevegleds@gmail.com>
2019-04-09 20:42:53 +0100

    09 April 2019 Changed format of columns in html and uploaded to http://gledhill.xyz/sg/running/raceresults.html Need to make climb and effort integers ************

--- raceresults/raceresults.py
+++ raceresults/raceresults.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 data_dir = os.getcwd()
@@ -9,16 +10,20 @@ def pace(time):
     return pd.to_timedelta(time) / 2
 
 
-def main():
+def get_race_results():
     dfraceresults = pd.read_excel(results_file, encoding='ISO-8859-1')
+    dfraceresults['Date'] = pd.to_datetime(dfraceresults['Date']).dt.strftime("%d %b %y")
+    # dfraceresults['Climb FT'] = dfraceresults['Climb FT'].astype(int)
     print(dfraceresults.head())
     print("results file:", results_file)
-    dfraceresults.to_html("raceresults.html")
+    # dfraceresults.to_html("raceresults.html")
     response = input("Calculate pace? Y/y \n")
     if response.lower() == 'y':
-        dfraceresults['Pace'] = pd.to_timedelta(dfraceresults.Time) / dfraceresults.Miles
-        dfraceresults.to_html("raceresults.html")
+        dfraceresults['Pace'] = (pd.to_timedelta(dfraceresults.Time) / dfraceresults.Miles)
+        dfraceresults['Pace'] = pd.to_datetime(dfraceresults['Pace']).dt.strftime("%M:%S")
+    dfraceresults_html = dfraceresults.round({"Climb FT": 0, "Miles": 2,  "Pace": 0})
+    dfraceresults_html.to_html("raceresults.html")
 
 
 if __name__ == "__main__":
-    main()
+    get_race_results()
