a3f9f1742ef9d940491316bec84008a9671bd645
stevegleds <stevegleds@gmail.com>
2019-05-04 13:30:30 +0100

    04 May 2019 Added mui styling and buttons

--- raceresults/raceresults.py
+++ raceresults/raceresults.py
@@ -12,27 +12,29 @@ pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
 
 html_string = '''
 <html>
-  <head><title>Race Results</title></head>
+  <head>
+      <!-- Start Mui boilerplate code - used for mui css -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- load MUI -->
+      <link href="//cdn.muicss.com/mui-0.9.39/css/mui.min.css" rel="stylesheet" type="text/css" />
+      <script src="//cdn.muicss.com/mui-0.9.39/js/mui.min.js"></script>
+      <!-- End mui boilerplate code -->
+  <title>Race Results</title></head>
   <link rel="stylesheet" type="text/css" href="df_style.css"/>
-  <table border="1" class="dataframe">
-  <thead>
-    <tr style="text-align: right;">
-      <th>Results</th>
-      <th>Sort By Pace</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><a href="my_race_results.html">My Race Results</a></td>
-      <td><a href="pace_my_race_results.html">Sorted by Pace</a></td>
-    </tr>
-        <tr>
-      <td><a href="others_race_results.html">Other Race Results</a></td>
-      <td><a href="pace_others_race_results.html">Sorted by Pace</a></td>
-    </tr>
-  </tbody>
-</table>
+  </head>
   <body>
+      <div class="mui-panel">Steve
+        <button class="mui-btn mui-btn--raised"><a href="my_race_results.html">Sort by Date</a></a> </button>
+        <button class="mui-btn mui-btn--raised"><a href="pace_my_race_results.html">Sort by Pace</a> </button>
+    </div>
+    <hr>
+          <div class="mui-panel">Others
+        <button class="mui-btn mui-btn--raised"><a href="others_race_results.html">Sort by Date</a></a> </button>
+        <button class="mui-btn mui-btn--raised"><a href="pace_others_race_results.html">Sort by Pace</a> </button>
+    </div>
+    <hr>
+   <body>
     {table}
   </body>
 </html>.
